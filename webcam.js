2e656a955d71b5ba1c5427f39c7dda873278aeaa
Deep Patel <deepp3021@gmail.com>
2021-05-19 18:12:05 +0530

    First Commit

---
+++ webcam.js
@@ -0,0 +1,27 @@
+const videoEL = document.getElementById("webcam-vid");
+const screenshotEL = document.getElementById("screenshots");
+
+let imageCapture ;
+
+
+
+
+const startWebcam = () =>{
+    if(navigator.mediaDevices.getUserMedia){
+        navigator.mediaDevices.getUserMedia({video:true}).then(stream =>{
+            videoEL.srcObject = stream
+            imageCapture = new ImageCapture(stream.getVideoTracks()[0])
+        })
+        .catch(err => {
+            console.log("Somthing went wrong",err);
+        });
+    }
+}
+
+const takeScreenshot = () => {
+    imageCapture.takePhoto().then(blob => {
+        const img = document.createElement('img')
+        img.src = window.URL.createObjectURL(blob)
+        screenshotEL.appendChild(img)
+    })
+}
